aaa8f7368507084bb54aa0c5ef02b4988ba6e4de
Shiueh Liming <slee@tw.augustatek.com>
2011-12-01 10:49:38 +0800

    backup

---
+++ devp/hook-git-rollback.sh
@@ -0,0 +1,3 @@
+#!/bin/bash -x
+time_stamp=$1
+
--- devp/hook-git.sh
+++ devp/hook-git.sh
@@ -1,19 +1,20 @@
-#!/bin/bash
+#!/bin/bash -x
 
-#top_src_list="platform kernel device"
-top_src_list="kernel"
+top_src_list="platform kernel device"
+#top_src_list="kernel"
 base_dir="/data2/git/android"
+time_stamp=`date +%Y%m%d.%H%M`
 for top_src in $top_src_list
 do
 	find_hook_list=$(find $base_dir/$top_src/. -type d -name 'hooks')
 	for git_hook_folder in $find_hook_list
 	do
+		cd $git_hook_folder
 		if [ -f $git_hook_folder/pre-receive ]; then
-			cd $git_hook_folder
-			echo "mv $git_hook_folder/pre-receive $git_hook_folder/pre-receive-`date +%Y%m%d.%H%M`"
-			echo "ln -s /data2/git/android/githook/githook/release/pre-receive"
-			echo "ln -s /data2/git/android/githook/githook/release/branch_req_pr_list"
-			cd -
+			echo "mv $git_hook_folder/pre-receive $git_hook_folder/pre-receive-backup-$time_stamp"
 		fi
+		echo "ln -s /data2/git/android/githook/githook/release/pre-receive"
+		echo "ln -s /data2/git/android/githook/githook/release/branch_req_pr_list"
+		cd -
 	done
 done
